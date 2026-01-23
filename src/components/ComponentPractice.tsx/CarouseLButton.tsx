df1b40402ef155c9ecacc4c10e43d8ec3fb4291f
Nihility <rbfmatoza@mymail.mapua.edu.ph>
2026-01-23 21:59:52 +0800

    chore: add types of buttons

--- src/components/ComponentPractice.tsx/CarouseLButton.tsx
+++ src/components/ComponentPractice.tsx/CarouseLButton.tsx
@@ -12,15 +12,16 @@ const CarouseLButton = () => {
   return (
     <div>
       <div className="Circular Buffer Indexing">
-        <button
-          className="flex border-2 p-2 rounded-2xl"
+        <div
+          className={`flex border-2 p-2 m-2 h-100 w-100 rounded-2xl ${Colors[index]}`}
+        >
+                    <button
+          className="flex border-2 p-2 rounded-2xl h-fit bg-white "
           onClick={() => nextColor()}
         >
           Circular Indexing
         </button>
-        <div
-          className={`flex border-2 p-2 m-2 h-100 w-100 rounded-2xl ${Colors[index]}`}
-        ></div>
+        </div>
       </div>
     </div>
   );
