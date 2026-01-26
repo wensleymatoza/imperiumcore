78fceaa0d3c3e4ab717ab0afaa8032a0e398bb2e
Nihility <rbfmatoza@mymail.mapua.edu.ph>
2026-01-26 21:16:00 +0800

    chore: start to plan for merging figma to code design

--- src/routes/pages/Buttons/index.tsx
+++ src/routes/pages/Buttons/index.tsx
@@ -5,8 +5,7 @@ export const Route = createFileRoute('/pages/Buttons/')({
 })
 
 function RouteComponent() {
-  return <div className='flex border-2 min-h-svh p-2 bg-gray-700 w-screen flex-col'>
-    
+  return <div className='flex border-2 min-h-svh p-2 bg-gray-700 w-screen flex-col'> 
     <span className='text-2xl text-[#F0F0F0] mb-2'><h1>Components</h1></span>
     <div className='flex bg-slate-500 flex-1 rounded-2xl p-2 '>
     <a className='flex border-2 p-2 m-2 bg-gray-500 rounded-2xl h-50 w-50 justify-center items-center' href='/pages/Buttons/ButtonExample'>
@@ -15,5 +14,3 @@ function RouteComponent() {
     </div>
   </div>
 }
-
-
